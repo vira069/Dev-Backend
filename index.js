6ac96c27f3b24f259af025419fbde3ce613697e3
vira069 <vira.kumar069@gmail.com>
2024-05-29 13:21:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req, res) =>{
+    res.send('ravi.com')
+})
+
+app.get('/login',(req, res) =>{
+    res.send('<h1>Welcome! login please </h1>');
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
